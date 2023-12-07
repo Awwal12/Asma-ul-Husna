9103af11dc4fc7b7ac1b6c5c2a8df18434397a90
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-12-07 15:09:15 +0100

    added card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script>
-  import Greet from '../lib/Greet.svelte'
+	import Cards from './Cards.svelte';
+  import Card from './Cards.svelte'; 
 </script>
+<div class="main-content">
+  <!-- Your main page content goes here -->
 
+  <!-- Include the Card component -->
+  <Cards />
+</div>
 
-<Greet />
+<style>
+  /* Add any page-specific styling here */
+  .main-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>

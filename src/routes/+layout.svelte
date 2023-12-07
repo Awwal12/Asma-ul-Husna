f2ebb1aacbaa4e0b42cef967e265b748906aea9a
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-12-07 15:08:25 +0100

    syntax update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,26 +3,22 @@
 </script>
 
 <style>
-  /* Set your desired background color */
   :global(body) {
-    background-color: #3498db; /* Replace with your preferred color code */
-    margin: 0; /* Reset default body margin */
+    background-color: #3498db; 
+    margin: 0; 
     box-shadow: inset 0 0 10px rgba(255, 255, 255, 0.2);
-    font-family: 'Arial', sans-serif; /* Optional: Set a default font-family */
+    font-family: 'Arial', sans-serif; 
   }
-    /* Center the app header */
+    /* Center of the app header */
   .app-header {
-    background-color: #4682b4; /* Adjust the background color as needed */
+    background-color: #4682b4; 
   }
 </style>
   <div class="text-center p-4 bg-gray-700 text-white app-header">
     <h1 class="text-3xl font-bold">Asma-ul-Husna</h1>
     <h2 class="text-xl font-bold">The 99 names and attributes of Allah (S.W.T)</h2>
-    <!-- Add additional header content as needed -->
   </div>
 
 
 <slot/>
 
-
-<slot/>
